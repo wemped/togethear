b98337de7ba23477336e98cfcc78e39d9d95ee1a
pruhter9 <ruhter.philip@gmail.com>
2015-08-19 14:30:05 -0700

    preparing for merge

--- client/javascripts/controllers/users.js
+++ client/javascripts/controllers/users.js
@@ -1,6 +1,13 @@
 togethear_app.controller('UserController',function ($scope,UserFactory,$location){
     var my = this;
+    $scope.visibility = 'hidden';
     my.err = '';
+    my.user_login_page = function (){
+        $location.path('/login');
+    };
+    my.dj_login_page = function (){
+        $location.path('/dj_login');
+    }
     my.login = function (){
         UserFactory.login(my.log);
     };
